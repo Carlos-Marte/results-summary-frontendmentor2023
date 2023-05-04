140f11eb25d96b98950717388a5fc7230e809678
Carlos-Marte <carlosmartedev@hotmail.com>
2023-05-04 17:17:07 -0400

    Agregando componente Results

--- src/components/ResultsSummary/Results.jsx
+++ src/components/ResultsSummary/Results.jsx
@@ -4,14 +4,14 @@ export const Results = () => {
       <p className='Results-paragraph'>Your result</p>
 
       <div className='Results-score'>
-        <p className='Results-paragraph'>
-          <span className='Results-spanBig'>76</span> of 100
+        <span className='Results-spanBig'>76</span>
+        <p className='Results-paragraph Results-paragraph--paleBlueColor'> of 100
         </p>
       </div>
 
       <div className='Results-texts'>
         <h2 className='Results-heading2'>Great</h2>
-        <p className='Results-paragraph'>
+        <p className='Results-paragraph Results-paragraph--paleBlueColor'>
           You scored higher than 65% of the people who have taken these tests.
         </p>
       </div>
